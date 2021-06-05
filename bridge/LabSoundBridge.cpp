bc99abb3a44f681d2dc056c6ba6f7abb099a4691
xioxin <i@xioxin.com>
2021-06-05 20:21:25 +0800

    upload

--- bridge/LabSoundBridge.cpp
+++ bridge/LabSoundBridge.cpp
@@ -5,14 +5,6 @@
 #include "dart_api/dart_native_api.h"
 #include "dart_api/dart_api_dl.h"
 
-#if defined(__ANDROID__) 
-#include <android/log.h>
-#define LOG(fmt, args...) __android_log_print(ANDROID_LOG_INFO, "LabSound", fmt, ##args)
-#define LOG_INFO(fmt, args...) __android_log_print(ANDROID_LOG_INFO, "LabSound", fmt, ##args)
-#define LOG_ERROR(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, "LabSound", fmt, ##args)
-#endif
-
-using namespace lab;
 #include "test.cpp"
 #include "KeepNode.cpp"
 #include "AudioBus.cpp"
@@ -34,15 +26,8 @@ using namespace lab;
 #include "AudioHardwareDeviceNode.cpp"
 #include "DynamicsCompressorNode.cpp"
 
+using namespace lab;
+
 DART_EXPORT void labTest() {
 
-    LOG_INFO("test");
-    
-    {
-        auto context = MakeRealtimeAudioContext(AudioStreamConfig(), AudioStreamConfig());
-    }
-
-    {
-        auto context2 = MakeRealtimeAudioContext(AudioStreamConfig(), AudioStreamConfig()).release();
-    }
 }
