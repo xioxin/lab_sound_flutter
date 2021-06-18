1bc369a6ea7700567040b538479de7f1feb8f8dd
xioxin <i@xioxin.com>
2021-06-18 09:59:10 +0800

    add Example ,Fix DelayNode

--- example/lib/labsound/ex_simple.dart
+++ example/lib/labsound/ex_simple.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/services.dart';
 import 'package:lab_sound_flutter/lab_sound_flutter.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:open_file/open_file.dart';
 
 List<AudioStreamConfig?> getDefaultAudioDeviceConfiguration(
     [bool withInput = false]) {
@@ -110,3 +111,175 @@ class ExSimple2 {
   }
 }
 
+class ExOscPop {
+  play() async {
+    final defaultAudioDeviceConfigurations =
+    getDefaultAudioDeviceConfiguration();
+    final context = AudioContext(
+        outputConfig: defaultAudioDeviceConfigurations.last,
+        inputConfig: defaultAudioDeviceConfigurations.first);
+
+    final oscillator = OscillatorNode(context);
+    final recorder = RecorderNode.fromConfig(context, defaultAudioDeviceConfigurations.last!);
+    final gain = GainNode(context);
+
+    gain.gain.value = 1;
+    oscillator.connect(gain);
+    gain.connect(context.device);
+
+    oscillator.frequency.value = 1000;
+    oscillator.type = OscillatorType.sine;
+    context.addAutomaticPullNode(recorder);
+    recorder.startRecording();
+    gain.connect(recorder);
+
+    for (double i = 0; i < 5.0; i += 1.0)
+    {
+      oscillator.start(when: 0);
+      oscillator.stop(when: 0.5);
+      await Future.delayed(Duration(seconds: 1));
+    }
+
+    recorder.stopRecording();
+    context.removeAutomaticPullNode(recorder);
+
+    final tempDir = await getTemporaryDirectory();
+    final file = File(tempDir.path + '/ex_osc_pop.wav');
+    if(file.existsSync()) file.deleteSync();
+
+    recorder.writeRecordingToWav(file.path);
+    context.disconnect(context.device);
+    await Future.delayed(Duration(milliseconds: 100));
+    context.dispose();
+  }
+}
+
+
+class ExPlaybackEvents {
+  play() async {
+    final defaultAudioDeviceConfigurations =
+    getDefaultAudioDeviceConfiguration();
+    final context = AudioContext(
+        outputConfig: defaultAudioDeviceConfigurations.last,
+        inputConfig: defaultAudioDeviceConfigurations.first);
+
+    final musicClip = await makeBusFromSampleFile("stereo-music-clip.wav");
+
+    final sampledAudio = AudioSampleNode(context);
+    sampledAudio.setBus(musicClip);
+    sampledAudio.connect(context.device);
+    sampledAudio.schedule(relativeWhen: 0);
+
+    sampledAudio.onEnded.listen((event) {
+      print("sampledAudio finished...");
+    });
+    sampledAudio.schedule(relativeWhen: 0.0);
+    await Future.delayed(Duration(seconds: 6));
+    context.dispose();
+  }
+}
+
+
+class ExOfflineRendering {
+  play() async {
+
+    final offlineConfig = AudioStreamConfig(deviceIndex: 0, desiredSampleRate: 48000, desiredChannels: 2);
+    final double recordingTimeMs = 1000;
+
+    final context = AudioContext.offline(outputConfig: offlineConfig);
+
+    final oscillator = OscillatorNode(context);
+    final musicClip = await makeBusFromSampleFile("stereo-music-clip.wav");
+    final musicClipNode = AudioSampleNode(context);
+    final gain = GainNode(context);
+
+    final recorder = RecorderNode.fromConfig(context, offlineConfig);
+
+    context.addAutomaticPullNode(recorder);
+
+    recorder.startRecording();
+
+    gain.gain.value = 0.125;
+
+    oscillator.connect(gain);
+    gain.connect(recorder);
+
+    oscillator.frequency.value = 880;
+    oscillator.type = OscillatorType.sine;
+    oscillator.start();
+
+    musicClipNode.connect(recorder);
+    musicClipNode.setBus(musicClip);
+    musicClipNode.schedule();
+
+    await context.startOfflineRendering();
+
+    recorder.stopRecording();
+    context.removeAutomaticPullNode(recorder);
+
+
+    final tempDir = await getTemporaryDirectory();
+    final file = File(tempDir.path + '/ex_osc_pop.wav');
+    if(file.existsSync()) file.deleteSync();
+
+    recorder.writeRecordingToWav(file.path);
+
+    OpenFile.open(file.path);
+
+    context.dispose();
+  }
+}
+
+
+// This demonstrates the use of `connectParam` as a way of modulating one node through another.
+// Params are effectively control signals that operate at audio rate.
+class ExTremolo {
+  play() async {
+    // todo connectParam miss
+  }
+}
+
+
+class ExFrequencyModulation {
+  play() async {
+    final defaultAudioDeviceConfigurations =
+    getDefaultAudioDeviceConfiguration();
+    final context = AudioContext(
+        outputConfig: defaultAudioDeviceConfigurations.last,
+        inputConfig: defaultAudioDeviceConfigurations.first);
+
+    final modulator = OscillatorNode(context);
+    final modulatorGain = GainNode(context);
+    final osc = OscillatorNode(context);
+    final trigger = ADSRNode(context);
+
+    final signalGain = GainNode(context);
+    final feedbackTap = GainNode(context);
+    final chainDelay = DelayNode(context);
+
+
+    modulator.type = OscillatorType.square;
+    modulator.start();
+
+    osc.type = OscillatorType.square;
+    osc.frequency.value = 300;
+    osc.start();
+
+    signalGain.gain.value = 1;
+    feedbackTap.gain.value = 0.5;
+
+    chainDelay.delayTime.value = 0;
+
+
+    // todo: connectParam miss;
+
+  }
+}
+
+// In most examples, nodes are not disconnected during playback. This sample shows how nodes
+// can be arbitrarily connected/disconnected during runtime while the graph is live.
+class ExRuntimeGraphUpdate {
+  play() async {
+
+  }
+}
--- lib/src/lab_sound/delay_node.dart
+++ lib/src/lab_sound/delay_node.dart
@@ -1,16 +1,17 @@
 import 'audio_context.dart';
 import 'audio_node.dart';
 import 'audio_param.dart';
+import 'audio_setting.dart';
 import 'lab_sound.dart';
 
 class DelayNode extends AudioNode {
-  late AudioParam delayTime;
+  late AudioSettingFloat delayTime;
   DelayNode(AudioContext ctx): super(ctx, LabSound().createDelayNode(ctx.pointer)) {
-    this.delayTime = AudioParam(this.ctx, this.nodeId, LabSound().DelayNode_delayTime(this.nodeId));
+    this.delayTime = AudioSettingFloat(this.ctx, this.nodeId, LabSound().DelayNode_delayTime(this.nodeId));
   }
 
   DelayNode.fromId(AudioContext ctx, int id): super(ctx, id) {
-    this.delayTime = AudioParam(this.ctx, this.nodeId, LabSound().DelayNode_delayTime(this.nodeId));
+    this.delayTime = AudioSettingFloat(this.ctx, this.nodeId, LabSound().DelayNode_delayTime(this.nodeId));
   }
 
 }
