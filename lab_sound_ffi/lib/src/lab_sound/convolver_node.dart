48ec5e599bb4c4ab3e3d4cf304297e2c5216907d
xioxin <i@xioxin.com>
2022-03-08 00:37:46 +0800

    add ConvolverNode imspector

--- lab_sound_ffi/lib/src/lab_sound/convolver_node.dart
+++ lab_sound_ffi/lib/src/lab_sound/convolver_node.dart
@@ -1,13 +1,17 @@
 import 'package:lab_sound_ffi/lab_sound_ffi.dart';
 
-
 class ConvolverNode extends AudioNode {
   late AudioParam delayTime;
-  ConvolverNode(AudioContext ctx): super(ctx, LabSound().createConvolverNode(ctx.pointer));
+  AudioBus? impulse;
+  ConvolverNode(AudioContext ctx)
+      : super(ctx, LabSound().createConvolverNode(ctx.pointer));
 
   bool get normalize => LabSound().ConvolverNode_normalize(nodeId) > 0;
-  set normalize(bool v) => LabSound().ConvolverNode_setNormalize(nodeId, v ? 1 : 0);
+  set normalize(bool v) =>
+      LabSound().ConvolverNode_setNormalize(nodeId, v ? 1 : 0);
 
-  setImpulse(AudioBus bus) => LabSound().ConvolverNode_setImpulse(nodeId, bus.resourceId);
-
+  setImpulse(AudioBus bus) {
+    impulse = bus;
+    LabSound().ConvolverNode_setImpulse(nodeId, bus.resourceId);
+  }
 }
